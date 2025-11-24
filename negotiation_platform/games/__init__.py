488c54743072d1265cb8bf875353c0adf2d18cca
TomZiegler2808 <97045511+TomZiegler2808@users.noreply.github.com>
2025-11-24 13:44:50 +0100

    changed imports to new names

--- negotiation_platform/games/__init__.py
+++ negotiation_platform/games/__init__.py
@@ -1,7 +1,7 @@
 from .base_game import BaseGame
-from .price_bargaining_guidance_free import CompanyCarGame
-from .resource_exchange_car_structure_guidance_free import ResourceAllocationGame
-from .integrative_negotiation_guidance_free import IntegrativeNegotiationsGame
+from .price_bargaining import CompanyCarGame
+from .resource_allocation import ResourceAllocationGame
+from .integrative_negotiation import IntegrativeNegotiationsGame
 
 __all__ = [
     'BaseGame', 'CompanyCarGame', 'ResourceAllocationGame',
